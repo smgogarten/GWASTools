044e95837cd0114d9a1873f3be8470b2a0cddeab
Stephanie Gogarten <sdmorris@u.washington.edu>
2013-04-23 20:52:38 +0000

    changed deprecated functions to defunct
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/GWASTools@75958 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/pedigreeClean.R
+++ R/pedigreeClean.R
@@ -10,47 +10,7 @@
 
 pedigreeClean<-function(pedigree)
 {
-  .Deprecated("pedigreeCheck")
-  
-	cl<-unlist(lapply(pedigree,class))
-      if(is.element("factor",cl)) stop("some variables are factors")	
-
-	## Finding entries that are missing
-      wfam<-which(is.na(pedigree$family) | is.element(pedigree$family,""))
-      wind<-which(is.na(pedigree$individ) | is.element(pedigree$individ,""))
-      wfa<-which(is.na(pedigree$father) | is.element(pedigree$father,""))
-      wmo<-which(is.na(pedigree$mother) | is.element(pedigree$mother,""))
-	
-	
-	##Checking sex code
-	rows.sexcode.error<-which(!is.element(pedigree$sex,c("M","F")))
-	
-	##Checking individual id's of 0
-	zero.individ<-which(pedigree$individ %in% 0)
-	
-	##Checking that same person is not both mother and father
-	mofa<-NULL
-	u<-unique(pedigree$family[!is.na(pedigree$family) & !is.element(pedigree$family,"")])
-      mf<-list()
-      cnt<-1
-	for (i in u){tsamp<-pedigree[is.element(pedigree$family,i),]
-	  mo<-unique(tsamp$mother[!is.element(tsamp$mother,0)&!is.na(tsamp$mother) & !is.element(tsamp$mother, "")])
-	  fa<-unique(tsamp$father[!is.element(tsamp$father,0)&!is.na(tsamp$father) & !is.element(tsamp$father,"")])
-	  prob<-intersect(mo,fa)
-	  if(length(prob)!=0){mofa<-c(mofa,i); mf[[cnt]]<-prob;cnt<-cnt+1}
-      }
-      if(length(mofa)!=0) names(mf)<-mofa
-
- 
-      out<-list(wfam,wind,wfa,wmo,rows.sexcode.error,zero.individ)
-      lens<-unlist(lapply(out,length))
-      mfln<-length(mf)
-      if(all(lens==0) & mfln==0){
-		out.list <- NULL;return(out.list)} else {
-		out.list<-list(wfam,wind,wfa,wmo,rows.sexcode.error,zero.individ,mf)
-		names(out.list)<-c("family.missing","individ.missing","father.missing","mother.missing","sexcode.error","zero.individ","both.mother.father");
-		return(out.list)
-      }
+  .Defunct("pedigreeCheck")
 }
 
 
--- R/pedigreeFindDuplicates.R
+++ R/pedigreeFindDuplicates.R
@@ -9,43 +9,5 @@
 
 pedigreeFindDuplicates<-function(pedigree, verbose=TRUE) 
 {
-  .Deprecated("pedigreeCheck")
-  
-	er<-pedigreeClean(pedigree)
-      if(!is.null(er)) stop("basic errors - run pedigreeClean to identify")
-
-	###Check duplicates within family
-	dup<-data.frame()
-	u<-unique(pedigree$family)
-	un<-length(u)
-	for(i in 1:un) {
-	  fsamp<-pedigree[is.element(pedigree$family,u[i]),c("individ","mother","father","sex")] #get info for family i
-	  ui<-unique(fsamp$individ)
-	  uin<-length(ui)
-	  if(uin <1) stop("error")
-	   for( j in 1:uin) {
-		 x<-fsamp[is.element(fsamp$individ,ui[j]), c("mother","father","sex")] #get rows for individual j in family i
-		 if(dim(x)[1]<=1) next
-		 chk<-all(length(unique(x$mother))==1 && length(unique(x$father))==1 && length(unique(x$sex))==1)
-		 if(chk==FALSE) dup<-rbind(dup,c(u[i],ui[j],dim(x)[1],1)) else dup<-rbind(dup,c(u[i],ui[j],dim(x)[1],0))
-		  
-		}
-	 }
-	if(dim(dup)[1]==0) {
-	  if (verbose)
-		  message("There are no duplicates")
-	  out.list<- NULL
-	} 
-	else 
-	{
-	names(dup)<-c("family","individ","copies","match")
-	dup$match[is.element(dup$match,0)]<-"Yes"
-	dup$match[is.element(dup$match,1)]<-"No"
-	
-	dups.match<-dup[is.element(dup$match,"Yes"),c("family","individ","copies")]
-	dups.mismatch<-dup[is.element(dup$match,"No"),c("family","individ","copies")]
-	
-	out.list<-list(dups.mismatch,dups.match); names(out.list)<-c("dups.mismatch", "dups.match")
-	}
-	return(out.list)
+  .Defunct("pedigreeCheck")
 }
