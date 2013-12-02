e5bf595dfb494dc206814fb67801e3c79aed69d4
Adrienne Stilp <amstilp@uw.edu>
2013-12-02 22:37:30 +0000

    updated color scheme to match manhattan plot
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/GWASTools@83820 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/snpCorrelationPlot.R
+++ R/snpCorrelationPlot.R
@@ -1,33 +1,39 @@
 
 snpCorrelationPlot<-function(correlations,
-                               chromosome,
-                               ylim=c(0,1),
-                               ylab="abs(correlation)",
-                               ...){
-
-	# correlations is a vector of correlations between SNPs and a given PC
-	# chromosome is a parallel vector of chromosome IDs (chrom.int)
-	# v2 makes pch="." and removes cex=0.3
-	
-        stopifnot(length(correlations) == length(chromosome))
-        
-	N <- length(correlations)
-	y <- correlations
-
-        chrom.labels <- unique(chromosome)
-	chrom.int <- as.numeric(factor(chromosome, levels=chrom.labels))
-   	chromstart <- which(c(1,diff(chrom.int)) == 1) # element numbers for the first SNP on each chromosome
-        chromend <- c(chromstart[-1],N)  # element numbers for the last SNP on each chromosome
-   	x <- (1:N) + chrom.int*(chromend[1]/6) # uniform spacing for SNP positions with offset
-           
-        plot(x, y, col=chrom.int, pch=".",
-             bg=chrom.int, xlab="Chromosome", ylab=ylab,
-             xaxt="n", ..., ylim=ylim)
-
-        centers <- (x[chromstart]+x[chromend]-(chromend[1]/6))/2
-	centers[length(chrom.labels)] <- x[N] # puts the last tick at the position of last snp
-        axis(1, at=centers, labels=chrom.labels, las=2)
-    }
+                             chromosome,
+                             ylim=c(0,1),
+                             ylab="abs(correlation)",
+                             ...){
+  
+  # correlations is a vector of correlations between SNPs and a given PC
+  # chromosome is a parallel vector of chromosome IDs (chrom.int)
+  # v2 makes pch="." and removes cex=0.3
+  
+  stopifnot(length(correlations) == length(chromosome))
+  
+  N <- length(correlations)
+  y <- correlations
+  
+  chrom.labels <- unique(chromosome)
+  chrom.int <- as.numeric(factor(chromosome, levels=chrom.labels))
+  chromstart <- which(c(1,diff(chrom.int)) == 1) # element numbers for the first SNP on each chromosome
+  chromend <- c(chromstart[-1],N)  # element numbers for the last SNP on each chromosome
+  x <- (1:N) + chrom.int*(chromend[1]/6) # uniform spacing for SNP positions with offset
+  
+  # colors from colorbrewer Dark2 map, 8 levels
+  chrom.col <- factor(chrom.int)
+  levels(chrom.col) <- rep_len(c("#1B9E77", "#D95F02", "#7570B3", "#E7298A", "#66A61E", "#E6AB02", "#A6761D", "#666666"), length(levels(chrom.col)))
+  chrom.col <- as.character(chrom.col)
+  
+  
+  plot(x, y, col=chrom.col, pch=".",
+       bg=chrom.int, xlab="Chromosome", ylab=ylab,
+       xaxt="n", ..., ylim=ylim)
+  
+  centers <- (x[chromstart]+x[chromend]-(chromend[1]/6))/2
+  centers[length(chrom.labels)] <- x[N] # puts the last tick at the position of last snp
+  axis(1, at=centers, labels=chrom.labels, las=2)
+}
 
 
 
