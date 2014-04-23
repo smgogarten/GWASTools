da1a6a2e73ece06f4b861a6060aff347d3642c22
Adrienne Stilp <amstilp@uw.edu>
2014-04-23 17:06:41 +0000

    added thinThreshold argument to manhattanPlot and qqPlot functions to thin pvalues that are higher than a given threshold
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/GWASTools@89390 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/manhattanPlot.R
+++ R/manhattanPlot.R
@@ -1,50 +1,125 @@
-
 manhattanPlot <- function(p,
-		 chromosome,
-		 ylim = NULL,
-		 trunc.lines = TRUE,
-         signif = 5e-8,
-		 ...)
+                             chromosome,
+                             ylim = NULL,
+                             trunc.lines = TRUE,
+                             signif = 5e-8,
+                             thinThreshold=NULL,
+                             ...)
 {
-    stopifnot(length(p) == length(chromosome))
+  stopifnot(length(p) == length(chromosome))
   
-   	logp <- -log(p,10)
-    N <- length(logp)
-   	ymax <- log(N,10) + 4
-   	if (is.null(ylim)) {
-      ylim <- c(0,ymax)
-    } else { 
-      ymax <- ylim[2]
+  logp <- -log(p,10)
+  
+  # remove NAs
+  sel <- !is.na(logp)
+  logp <- logp[sel]
+  chromosome <- chromosome[sel]
+  
+  # select SNPs if thinning is requested
+  if (!is.null(thinThreshold)){
+    #quant <- quantile(logp[logp < thinThreshold], probs=1:10/10)
+    breaks <- seq(from=0, to=thinThreshold, length.out=11)
+    logp.cut <- cut(logp, breaks=breaks, right=FALSE)
+    ind.list <- list()
+    for (level in levels(logp.cut)){
+      sel <- which(logp.cut == level)
+      ind.list[[level]] <- sample(sel, min(5000, length(sel)))
     }
-
-    chrom.labels <- unique(chromosome)
-	chrom.int <- as.numeric(factor(chromosome, levels=chrom.labels))
-   	chromstart <- which(c(1,diff(chrom.int)) == 1) # element numbers for the first SNP on each chromosome
-    chromend <- c(chromstart[-1],N)  # element numbers for the last SNP on each chromosome
-   	x <- (1:N) + chrom.int*(chromend[1]/6) # uniform spacing for SNP positions with offset
+    ind.list[["max"]] <- which(logp >= thinThreshold)
+    ind <- unlist(ind.list, use.names=FALSE)
+    ind <- sort(ind) # sorting necessary for chromosome ordering
     
-    # colors from colorbrewer Dark2 map, 8 levels
-  	chrom.col <- factor(chrom.int)
-  	levels(chrom.col) <- rep_len(c("#1B9E77", "#D95F02", "#7570B3", "#E7298A", "#66A61E", "#E6AB02", "#A6761D", "#666666"), length(levels(chrom.col)))
-  	chrom.col <- as.character(chrom.col)
+    p <- p[ind]
+    logp <- logp[ind]
+    chromosome <- chromosome[ind]
+  } 
+  
+  N <- length(logp)
+  ymax <- log(N,10) + 4
+  if (is.null(ylim)) {
+    ylim <- c(0,ymax)
+  } else { 
+    ymax <- ylim[2]
+  }
+  
+  chrom.labels <- unique(chromosome)
+  chrom.int <- as.numeric(factor(chromosome, levels=chrom.labels))
+  chromstart <- which(c(1,diff(chrom.int)) == 1) # element numbers for the first SNP on each chromosome
+  chromend <- c(chromstart[-1],N)  # element numbers for the last SNP on each chromosome
+  x <- (1:N) + chrom.int*(chromend[1]/6) # uniform spacing for SNP positions with offset
+  
+  # colors from colorbrewer Dark2 map, 8 levels
+  chrom.col <- factor(chrom.int)
+  levels(chrom.col) <- rep_len(c("#1B9E77", "#D95F02", "#7570B3", "#E7298A", "#66A61E", "#E6AB02", "#A6761D", "#666666"), length(levels(chrom.col)))
+  chrom.col <- as.character(chrom.col)
+  
+  trunc <- FALSE
+  if (trunc.lines & any(logp > ymax, na.rm=TRUE)) trunc <- TRUE
+  y <- pmin(ymax,logp)
+  
+  plot(x, y, cex=0.3+y/ymax, col=chrom.col, pch=ifelse(y==ymax,24,19),
+       bg=chrom.int, xlab="Chromosome", ylab=quote(-log[10](p)),
+       xaxt="n", ..., ylim=ylim)
+  if (trunc) lines(c(min(x), max(x)), c(ymax,ymax), col = 'grey')
+  
+  centers <- (x[chromstart]+x[chromend]-(chromend[1]/6))/2
+  centers[length(chrom.labels)] <- x[N]  # puts the last tick at the position of last snp
+  axis(1, at=centers, labels=chrom.labels, las=2)
+  
+  # add a line for genome-wide significance
+  if (!is.null(signif)) {
+    abline(h=-log10(signif), lty=2, col="gray")
+  }
+}
 
-   	trunc <- FALSE
-   	if (trunc.lines & any(logp > ymax, na.rm=TRUE)) trunc <- TRUE
-    	y <- pmin(ymax,logp)
 
-        plot(x, y, cex=0.3+y/ymax, col=chrom.col, pch=ifelse(y==ymax,24,19),
-             bg=chrom.int, xlab="Chromosome", ylab=quote(-log[10](p)),
-             xaxt="n", ..., ylim=ylim)
-        if (trunc) lines(c(min(x), max(x)), c(ymax,ymax), col = 'grey')
-
-        centers <- (x[chromstart]+x[chromend]-(chromend[1]/6))/2
-        centers[length(chrom.labels)] <- x[N]  # puts the last tick at the position of last snp
-        axis(1, at=centers, labels=chrom.labels, las=2)
-
-        # add a line for genome-wide significance
-        if (!is.null(signif)) {
-          abline(h=-log10(signif), lty=2, col="gray")
-        }
- }
-
-
+# 
+# manhattanPlot <- function(p,
+# 		 chromosome,
+# 		 ylim = NULL,
+# 		 trunc.lines = TRUE,
+#          signif = 5e-8,
+# 		 ...)
+# {
+#     stopifnot(length(p) == length(chromosome))
+#   
+#    	logp <- -log(p,10)
+#     N <- length(logp)
+#    	ymax <- log(N,10) + 4
+#    	if (is.null(ylim)) {
+#       ylim <- c(0,ymax)
+#     } else { 
+#       ymax <- ylim[2]
+#     }
+# 
+#     chrom.labels <- unique(chromosome)
+# 	chrom.int <- as.numeric(factor(chromosome, levels=chrom.labels))
+#    	chromstart <- which(c(1,diff(chrom.int)) == 1) # element numbers for the first SNP on each chromosome
+#     chromend <- c(chromstart[-1],N)  # element numbers for the last SNP on each chromosome
+#    	x <- (1:N) + chrom.int*(chromend[1]/6) # uniform spacing for SNP positions with offset
+#     
+#     # colors from colorbrewer Dark2 map, 8 levels
+#   	chrom.col <- factor(chrom.int)
+#   	levels(chrom.col) <- rep_len(c("#1B9E77", "#D95F02", "#7570B3", "#E7298A", "#66A61E", "#E6AB02", "#A6761D", "#666666"), length(levels(chrom.col)))
+#   	chrom.col <- as.character(chrom.col)
+# 
+#    	trunc <- FALSE
+#    	if (trunc.lines & any(logp > ymax, na.rm=TRUE)) trunc <- TRUE
+#     	y <- pmin(ymax,logp)
+# 
+#         plot(x, y, cex=0.3+y/ymax, col=chrom.col, pch=ifelse(y==ymax,24,19),
+#              bg=chrom.int, xlab="Chromosome", ylab=quote(-log[10](p)),
+#              xaxt="n", ..., ylim=ylim)
+#         if (trunc) lines(c(min(x), max(x)), c(ymax,ymax), col = 'grey')
+# 
+#         centers <- (x[chromstart]+x[chromend]-(chromend[1]/6))/2
+#         centers[length(chrom.labels)] <- x[N]  # puts the last tick at the position of last snp
+#         axis(1, at=centers, labels=chrom.labels, las=2)
+# 
+#         # add a line for genome-wide significance
+#         if (!is.null(signif)) {
+#           abline(h=-log10(signif), lty=2, col="gray")
+#         }
+#  }
+# 
+# 
--- R/qqPlot.R
+++ R/qqPlot.R
@@ -1,35 +1,89 @@
-qqPlot <- function(pval, truncate = FALSE, ylim = NULL, ...)
+# qqPlot <- function(pval, truncate = FALSE, ylim = NULL, ...)
+# {
+# 	# pvalue is a vector of p-values
+# 	# truncate is T/F whether to truncate the y-axis (observed) to the same limits as the x-axis (expected)
+# 
+# 	pval <- -log10(sort(pval)) # sort() removes NAs
+# 	n <- length(pval)
+#         a <- 1:n
+#         b <- a/n
+#         upper <- qbeta(0.025, a, rev(a))
+#         lower <- qbeta(0.975, a, rev(a))
+# 	x <- -log10(b)
+# 
+# 
+#         char <- rep(1,n)
+#         if(!truncate){
+#           ylm <- ylim
+#           ylb <- expression(paste(-log[10], "(observed P)"))
+#         }else{
+#           maxx <- max(x)+2
+#           ylm <- c(0,maxx)
+#           ylb <- expression(paste(-log[10], "(observed P) - truncated"))
+#           nx <- length(which(pval > maxx))
+#           if(nx > 0){
+#             pval[1:nx] <- maxx
+#             char[1:nx] <- 2
+#           }
+#         }
+#         plot(x, pval, type = "n", ylim = ylm, ylab = ylb,
+#              xlab = expression(paste(-log[10], "(expected P)")), ...)
+#         polygon(-log10(c(b,rev(b))), -log10(c(upper, rev(lower))), density=NA, col="gray")
+#         points(x, pval, pch = char, ...)
+# 	abline(0,1,col="red")
+# }
+
+
+qqPlot <- function(pval, truncate = FALSE, ylim=NULL, thinThreshold=NULL, ...) 
 {
-	# pvalue is a vector of p-values
-	# truncate is T/F whether to truncate the y-axis (observed) to the same limits as the x-axis (expected)
-
-	pval <- -log10(sort(pval)) # sort() removes NAs
-	n <- length(pval)
-        a <- 1:n
-        b <- a/n
-        upper <- qbeta(0.025, a, rev(a))
-        lower <- qbeta(0.975, a, rev(a))
-	x <- -log10(b)
-
-
-        char <- rep(1,n)
-        if(!truncate){
-          ylm <- ylim
-          ylb <- expression(paste(-log[10], "(observed P)"))
-        }else{
-          maxx <- max(x)+2
-          ylm <- c(0,maxx)
-          ylb <- expression(paste(-log[10], "(observed P) - truncated"))
-          nx <- length(which(pval > maxx))
-          if(nx > 0){
-            pval[1:nx] <- maxx
-            char[1:nx] <- 2
-          }
-        }
-        plot(x, pval, type = "n", ylim = ylm, ylab = ylb,
-             xlab = expression(paste(-log[10], "(expected P)")), ...)
-        polygon(-log10(c(b,rev(b))), -log10(c(upper, rev(lower))), density=NA, col="gray")
-        points(x, pval, pch = char, ...)
-	abline(0,1,col="red")
+  # pvalue is a vector of p-values
+  # truncate is T/F whether to truncate the y-axis (observed) to the same limits as the x-axis (expected)
+  # thin is whether to thin insignificant p-values
+  
+  pval <- -log10(sort(pval)) # sort() removes NAs
+  n <- length(pval)
+  a <- 1:n
+  b <- a/n
+  x <- -log10(b)
+  
+  if (!is.null(thinThreshold)){
+    breaks <- seq(from=0, to=thinThreshold, length.out=11)
+    pval.cut <- cut(pval, breaks=breaks, right=FALSE)
+    #quant <- quantile(pval[pval < thinThreshold], probs=1:10/10)
+    #pval.cut <- cut(pval, breaks=c(0, quant), right=FALSE)
+    ind.list <- list()
+    for (level in levels(pval.cut)){
+      sel <- which(pval.cut == level)
+      ind.list[[level]] <- sample(sel, min(1000, length(sel)))
+    }
+    ind.list[["max"]] <- which(pval >= thinThreshold)
+    ind <- unlist(ind.list, use.names=FALSE)
+    ind <- sort(ind) # sorting necessary for polygon, below
+  } else {
+    ind <- 1:n
+  }
+  
+  upper <- qbeta(0.025, a, rev(a))
+  lower <- qbeta(0.975, a, rev(a))
+  
+  char <- rep(1,n)
+  if(!truncate){
+    ylm <- ylim
+    ylb <- expression(paste(-log[10], "(observed P)"))
+  }else{
+    maxx <- max(x)+2
+    ylm <- c(0,maxx)
+    ylb <- expression(paste(-log[10], "(observed P) - truncated"))
+    nx <- length(which(pval > maxx))
+    if(nx > 0){
+      pval[1:nx] <- maxx
+      char[1:nx] <- 2
+    }
+  }
+  plot(x[ind], pval[ind], type = "n", ylim = ylm, ylab = ylb,
+       xlab = expression(paste(-log[10], "(expected P)")), ...)
+  # upper and lower have already been subset
+  polygon(-log10(c(b[ind], rev(b[ind]))), -log10(c(upper[ind], rev(lower[ind]))), density=NA, col="gray")
+  points(x[ind], pval[ind], pch = char, ...)  
+  abline(0,1,col="red")  
 }
-
