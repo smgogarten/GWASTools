83c8edf31982a5bb72aeb82de003c1bbced21961
Adrienne Stilp <amstilp@uw.edu>
2015-04-17 21:16:37 +0000

    added ci option to remove confidence intervals in qqPlot function
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/GWASTools@102647 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/qqPlot.R
+++ R/qqPlot.R
@@ -1,40 +1,4 @@
-# qqPlot <- function(pval, truncate = FALSE, ylim = NULL, ...)
-# {
-# 	# pvalue is a vector of p-values
-# 	# truncate is T/F whether to truncate the y-axis (observed) to the same limits as the x-axis (expected)
-# 
-# 	pval <- -log10(sort(pval)) # sort() removes NAs
-# 	n <- length(pval)
-#         a <- 1:n
-#         b <- a/n
-#         upper <- qbeta(0.025, a, rev(a))
-#         lower <- qbeta(0.975, a, rev(a))
-# 	x <- -log10(b)
-# 
-# 
-#         char <- rep(1,n)
-#         if(!truncate){
-#           ylm <- ylim
-#           ylb <- expression(paste(-log[10], "(observed P)"))
-#         }else{
-#           maxx <- max(x)+2
-#           ylm <- c(0,maxx)
-#           ylb <- expression(paste(-log[10], "(observed P) - truncated"))
-#           nx <- length(which(pval > maxx))
-#           if(nx > 0){
-#             pval[1:nx] <- maxx
-#             char[1:nx] <- 2
-#           }
-#         }
-#         plot(x, pval, type = "n", ylim = ylm, ylab = ylb,
-#              xlab = expression(paste(-log[10], "(expected P)")), ...)
-#         polygon(-log10(c(b,rev(b))), -log10(c(upper, rev(lower))), density=NA, col="gray")
-#         points(x, pval, pch = char, ...)
-# 	abline(0,1,col="red")
-# }
-
-
-qqPlot <- function(pval, truncate = FALSE, ylim=NULL, thinThreshold=NULL, ...) 
+qqPlot <- function(pval, truncate = FALSE, ylim=NULL, thinThreshold=NULL, ci=TRUE, ...) 
 {
   # pvalue is a vector of p-values
   # truncate is T/F whether to truncate the y-axis (observed) to the same limits as the x-axis (expected)
@@ -63,8 +27,6 @@ qqPlot <- function(pval, truncate = FALSE, ylim=NULL, thinThreshold=NULL, ...)
     ind <- 1:n
   }
   
-  upper <- qbeta(0.025, a, rev(a))
-  lower <- qbeta(0.975, a, rev(a))
   
   char <- rep(1,n)
   if(!is.logical(truncate) | truncate){
@@ -89,7 +51,12 @@ qqPlot <- function(pval, truncate = FALSE, ylim=NULL, thinThreshold=NULL, ...)
   plot(x[ind], pval[ind], type = "n", ylim = ylm, ylab = ylb,
        xlab = expression(paste(-log[10], "(expected P)")), ...)
   # upper and lower have already been subset
-  polygon(-log10(c(b[ind], rev(b[ind]))), -log10(c(upper[ind], rev(lower[ind]))), density=NA, col="gray")
+  if (ci){
+    upper <- qbeta(0.025, a, rev(a))
+    lower <- qbeta(0.975, a, rev(a))
+
+    polygon(-log10(c(b[ind], rev(b[ind]))), -log10(c(upper[ind], rev(lower[ind]))), density=NA, col="gray")
+  }
   points(x[ind], pval[ind], pch = char, ...)  
   abline(0,1,col="red")  
 }
