06f2ce29fd2a8c3b4fb9cdc297a1034033871543
Stephanie Gogarten <sdmorris@u.washington.edu>
2016-05-17 21:02:57 +0000

    removed defunct functions, move deprecated to defunct
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/GWASTools@117596 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/defunct.R
+++ R/defunct.R
@@ -8,3 +8,12 @@ ncdfSubset <- function(...) {
 ncdfSubsetCheck <- function(...) {
     .Defunct("gdsSubsetCheck", msg="'ncdfSubsetCheck' is defunct with the removal of the ncdf package. Use 'convertNcdfGds' and 'gdsSubset' or ncdf4 package functions instead.")
 }
+
+
+plinkToNcdf <- function(...) {
+    .Defunct("snpgdsBED2GDS", msg="'plinkToNcdf' is deprecated. \nUse 'snpgdsBED2GDS' in package 'SNPRelate' instead.")
+}
+
+convertVcfGds <- function(...) {
+    .Defunct("snpgdsVCF2GDS", msg="'convertVcfGds' is deprecated. \nUse 'snpgdsVCF2GDS' in package 'SNPRelate' instead.")
+}
